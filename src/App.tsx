fcdfdac7526db0511ebfb4feaa33dd24afa37bf2
ch1ta <ng.phihoc123@gmail.com>
2025-03-09 13:56:20 +0700

    feat: add application form and effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import JobDetail from '@/app/pages/JobDetail/JobDetail';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './app/components/NavBar';
+import Footer from '@/app/components/Footer';
 import Homepage from './app/pages/Homepage';
 import RecruiterPage from './app/pages/recruiter/RecruiterPage';
-import Footer from '@/app/components/Footer';
+import JobDetail from '@/app/pages/JobDetail/JobDetail';
 
 export default function App() {
   return (
-    <div className='min-h-screen flex flex-col'>
-      <NavBar />
-      {/* <Homepage /> */}
-      {/* <RecruiterPage /> */}
-      <JobDetail/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className='min-h-screen flex flex-col'>
+        <NavBar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/recruiter" element={<RecruiterPage />} />
+            <Route path="/job/:id" element={<JobDetail />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
